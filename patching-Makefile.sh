160f8ffbd4ea37862785ccac27850245966947c9
AlirezaKm <alitm28@gmail.com>
2018-10-31 19:57:26 +0330

    added Patching file for Snort Makefile
    - Moved Makefile.dpdk from onvm-snort/snort-2.9.8.3/src to onvm-snort
    - Added patching script
     - in this script we can customize my installation paths of snort and daq and fixed dpdk and onvm

---
+++ patching-Makefile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+_PATCH_FILE_NAME=Makefile.patched
+_MAIN_FILE_TO_PATCH=Makefile.dpdk
+
+
+if [ ! -e $_MAIN_FILE_TO_PATCH ];then
+	echo "File '$_MAIN_FILE_TO_PATCH' not exits in current directory"
+	exit 10
+fi
+# Insert Directory of Cloned Repo
+read -p "onvm-snort cloned at [$(realpath `pwd`/..)] (example: /home/user): " _DIR || exit 10
+_DIR=${_DIR:-$(realpath `pwd`/..)}/onvm-snort/
+read -p "Snort will install to [/usr/local/] (example: /opt/snort): " _SNORT_INSTALL || exit 10
+_SNORT_INSTALL=${_SNORT_INSTALL:-/usr/local/}
+read -p "DAQ Install PATH [Using System as Default] (example: /opt/daq): " _DAQ_INSTALL || exit 10
+_DAQ_INSTALL=${_DAQ_INSTALL:- }
+
+# Create patched file
+cat $_MAIN_FILE_TO_PATCH | sed "s|/users/graceliu/snort/|$_DIR|g" | tee $_PATCH_FILE_NAME 1>/dev/null 2>&1 || exit 10
+
+# Replace new DIR
+sed -i "s|/users/graceliu/|$_DIR|g" $_PATCH_FILE_NAME || exit 10
+
+# Replace Prefix of Snort
+sed -i "s|prefix = /usr/local|prefix = $_SNORT_INSTALL|g" $_PATCH_FILE_NAME || exit 10
+
+if [ "$_DAQ_INSTALL" != " " ]; then
+	# Replace Libraries Path of DAQ
+	sed -i "s|-L/usr/local/lib|-L${_DAQ_INSTALL}/lib|g" $_PATCH_FILE_NAME || exit 10
+
+	# Replace CPPFLAGS
+	sed -i "s|CPPFLAGS = |CPPFLAGS = -I${_DAQ_INSTALL}/include |g" $_PATCH_FILE_NAME || exit 10
+
+	# Replace LDFLAGS
+	sed -i "s|LDFLAGS =  -lpcre -L/usr/lib -ldumbnet|LDFLAGS = -L${_DAQ_INSTALL}/lib -lpcre -ldumbnet|g" $_PATCH_FILE_NAME || exit 10
+fi
+
+echo "Patched file saved to '$_PATCH_FILE_NAME'"
+
